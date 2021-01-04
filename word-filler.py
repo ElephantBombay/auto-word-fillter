7eec793ee6c5d1c4fdc672a0de306b873bb2adf5
maximvallee <maximvallee@gmail.com>
2021-01-04 01:07:13 -0500

    clear git cache & added multi rows in the table

--- word-filler.py
+++ word-filler.py
@@ -6,18 +6,10 @@ import datetime
 
 
 def extract_from_csv(path_csv):
-    
     with open(path_csv) as csvDataFile:
         data = [row for row in csv.reader(csvDataFile)]
-        import pdb; pdb.set_trace()
-        date = data[1][0]
-        order = data[1][1]
-        volume = data[1][2]
-        price = data[1][3]
-
-    return date, order, volume, price
-
-
+        data.pop(0)
+    return data
 
 @click.command()
 @click.option('--name', prompt='Your name', help='Name of the report creator')
@@ -26,28 +18,20 @@ def extract_from_csv(path_csv):
 
 def fill_doc(name, last, company):
     csv_data = extract_from_csv("test.csv")
-    # import pdb; pdb.set_trace()
-    data = {
+    context = {
         "name": name,
         "last": last,
         "company": company,
         "created": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-        "order": csv_data[1],
-        "date": csv_data[0],
-        "volume": csv_data[2], 
-        "price": csv_data[3]
+        "table" : csv_data,
     }
-    # fill_doc("templates/my-template.docx", {"created": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),"name": name, "last": last, "company": company,})
-    # fill_doc("templates/my-template.docx", context)
 
+    # import pdb; pdb.set_trace()
     doc = DocxTemplate("templates/my-template.docx")
-    context = data
     doc.render(context)
     doc.save("output/generated_doc.docx")
-        # "company": company,
-        # "date": csv_data[0],
-        # "volume": csv_data[1], 
-        # "price": csv_data[2]
+    click.echo("Report complete!")
+
    
     
 if __name__ == '__main__':
